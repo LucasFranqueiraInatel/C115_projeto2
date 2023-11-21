8eedc99fab92a7f86c2e70199c10d39fe14e42ac
Lucas <lucas.f@gec.inatel.br>
2023-11-20 23:41:43 -0300

    update: script.py

--- script.py
+++ script.py
@@ -6,15 +6,11 @@ from mininet.link import TCLink
 
 
 def custom_topology():
-    "Create custom topology in Mininet."
 
-    # Initialize the network
     net = Mininet(controller=Controller, link=TCLink, switch=OVSSwitch)
 
-    # Add a controller
     c0 = net.addController('c0')
 
-    # Add hosts
     h1 = net.addHost('h1')
     h2 = net.addHost('h2')
     h3 = net.addHost('h3')
@@ -25,13 +21,11 @@ def custom_topology():
     h8 = net.addHost('h8')
     h9 = net.addHost('h9')
 
-    # Add switches
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
     s3 = net.addSwitch('s3')
     s4 = net.addSwitch('s4')
 
-    # Add links between switches and hosts
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(s1, s2)
@@ -45,13 +39,10 @@ def custom_topology():
     net.addLink(h8, s4)
     net.addLink(s4, h9)
 
-    # Start the network
     net.start()
 
-    # Run CLI (Command Line Interface)
     CLI(net)
 
-    # Stop the network
     net.stop()
 
 
